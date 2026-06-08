ebfa3e9e0e3560b598c28721a835e22f3e57c676
Denny228 <Dcadro@unipu.hr>
2026-06-08 21:05:21 +0200

    Add root App component

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <b-navbar
+      v-if="prijavljen"
+      toggleable="md"
+      type="dark"
+      variant="dark"
+      class="navbar-eventplanner"
+    >
+      <b-navbar-toggle target="nav-collapse" />
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Tri zone: balans lijevo, centar (linkovi + logo), Profil desno -->
+        <div class="navbar-triple w-100">
+          <div
+            class="navbar-zona navbar-zona-lijevo d-none d-md-block"
+            aria-hidden="true"
+          ></div>
+
+          <div class="navbar-zona navbar-zona-sredina">
+            <b-navbar-nav class="nav-grupa-centar">
+              <b-nav-item to="/dogadaji">Događaji</b-nav-item>
+            </b-navbar-nav>
+
+            <b-navbar-brand to="/" class="navbar-brand-centar">
+              <span class="logo-eventplanner logo-navbar">
+                <span class="logo-event">Event</span><span class="logo-planner">Planner</span>
+              </span>
+            </b-navbar-brand>
+
+            <b-navbar-nav class="nav-grupa-centar">
+              <b-nav-item to="/statistika">Statistika</b-nav-item>
+            </b-navbar-nav>
+          </div>
+
+          <div class="navbar-zona navbar-zona-desno">
+            <b-navbar-nav>
+              <b-nav-item to="/profil">Profil</b-nav-item>
+            </b-navbar-nav>
+          </div>
+        </div>
+      </b-collapse>
+    </b-navbar>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { auth } from "@/firebase";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      prijavljen: false,
+    };
+  },
+  mounted() {
+    // Navbar se prikazuje tek kad Firebase potvrdi prijavu
+    this.odjavaAuthPromatrac = auth.onAuthStateChanged((korisnik) => {
+      this.prijavljen = korisnik !== null;
+    });
+  },
+  beforeDestroy() {
+    if (this.odjavaAuthPromatrac) {
+      this.odjavaAuthPromatrac();
+    }
+  },
+  methods: {
+    async odjava() {
+      await auth.signOut();
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
