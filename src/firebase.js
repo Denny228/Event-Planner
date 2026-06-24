3d67cc5e26b83d3d3eca0e10a32c00a3ec8f5526
Denny Čadro <dennycadro@Mac.fritz.box>
2026-06-24 11:31:11 +0200

    Update Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 
 // Konfiguracija Firebase projekta EventPlanner
 const firebaseConfig = {
-  apiKey: "AIzaSyCv0_I247M10qR3LrNOVZqoHQkL9AUDqf4",
-  authDomain: "eventplanner-7f975.firebaseapp.com",
-  projectId: "eventplanner-7f975",
-  storageBucket: "eventplanner-7f975.firebasestorage.app",
-  messagingSenderId: "823496324194",
-  appId: "1:823496324194:web:5196a73bc8776abb164d3b"
+  apiKey: "AIzaSyC9j87eNuWeUiGPOX7zjjJafcdJ0wQoX5c",
+  authDomain: "eventplanner-30440.firebaseapp.com",
+  projectId: "eventplanner-30440",
+  storageBucket: "eventplanner-30440.firebasestorage.app",
+  messagingSenderId: "850175175625",
+  appId: "1:850175175625:web:f18b138b0260eb3e729618"
 };
 
 // Inicijalizacija Firebase aplikacije (v8 namespaced API)
